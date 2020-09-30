379c6232ec430297556314cd33494e4c469a8778
ngearhart <excelsiorvfx@gmail.com>
2020-09-29 22:55:47 -0400

    Started basic API framework

--- web/app.py
+++ web/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask("ESP Lights",
             static_folder="./frontend/dist/static",
             template_folder="./frontend/dist")
 
+@app.route('/setlights', methods=["POST"])
+def 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-
     return render_template("index.html")
--- web/frontend/src/views/Home.vue
+++ web/frontend/src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Junkyard Light Control</h1>
+    <form>
+      <div v-for="(value, name) in rooms" :key="name">
+        <label for="room7">{{name}}</label>
+        <v-select :options="value" label="name"></v-select>
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Dictionary } from 'vue-router/types/router'
+// import router from '../router'
+// import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import vSelect from 'vue-select'
+import 'vue-select/dist/vue-select.css'
+
+Vue.component('v-select', vSelect)
+
+export class Preset {
+  public r?: number
+  public g?: number
+  public b?: number
+
+  public name?: string
+
+  constructor (name: string, r?: number, g?: number, b?: number) {
+    this.name = name
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+}
 
 @Component({
   components: {
-    HelloWorld
+  //  HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  rooms: Dictionary<Array<Preset>> = {
+    '1st Floor Kitchen': [
+      new Preset('Warm White', 255, 255, 100),
+      new Preset('Cool White', 255, 255, 255),
+      new Preset('Off', 0, 0, 0)
+    ]
+  }
+}
 </script>
